d658f9e94945461083b88a55a125d3d5e311370e
Arkan <arkankhudiyev@gmail.com>
2023-09-26 19:05:30 +0400

    Changing string numbers with Number()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Baseball_Game_Task.
-var operation = [5,-2,4,"C","D",9,"+","+"]
+var operation = [5, -2, 4, "C", "D", 9, "+", "+"]   
 
 function callPoints(operation) {
     let newArr = []
@@ -7,27 +7,24 @@ function callPoints(operation) {
 
     for (let i = 0; i < operation.length; i++) {
         if (operation[i] !== "C" && operation[i] !== "D" && operation[i] !== "+") {
-            newArr.push(operation[i])
+            newArr.push(Number(operation[i]))
         }
         if (operation[i] === "C") {
             newArr.pop()
         }
         if (operation[i] === "D") {
-            let first = newArr.at(-1)
-            let add = first * 2
+            let add = newArr.at(-1) * 2
             newArr.push(add)
         }
         if (operation[i] === "+") {
-            let firstNumber = newArr.at(-1)
-            let secondNumber = newArr.at(-2)
-            let sum = firstNumber + secondNumber
+            let sum = newArr.at(-1) + newArr.at(-2)
             newArr.push(sum)
         }
 
     }
     // Ilk once hansi regemleri alirig console yazb baxmisam output (6) [5, -2, -4, 9, 5, 14]
-     console.log(newArr)
-    
+    console.log(newArr)
+
     //  next yeni newArr - i yeni bir for salib toplamisiq regemleri.
     for (let i = 0; i < newArr.length; i++) {
         sumOfNewArr += newArr[i]
